bf2be4954aae26bc74521d04409260566ecbe987
ligaopeng123 <gaopeng@123>
2022-10-10 11:48:35 +0800

    修改问题：修改颜色配置及约束定义 修改人：pgli 修改时间：2022年10月10日11:48:06

--- src/typing.ts
+++ src/typing.ts
@@ -1,15 +1,25 @@
 import { ReactNode } from "react";
 import { RouteProps } from "react-router-dom";
 
+/**
+ * 加载属性
+ */
+export type LoadingProps = {
+    loading?: boolean | ReactNode; // 是否使用loading效果  false不使用 true使用默认的 也可传递组件
+    loadingColor?: string; // 加载动画颜色
+}
+/**
+ * 加载器
+ */
+export type LoaderProps = {
+    isVite?: boolean; // 是否使用vite加载
+}
 export type RrefetchRoute = {
     prefetchComponent?: any; // 预加载资源
-    isVite?: boolean; // 是否使用vite加载
-    loading?: boolean | ReactNode; // 是否使用加载状态
-    loadingColor?: string; // 加载动画颜色
     component?: any; // pc端加载组件
     mComponent?: any; // 移动端加载组件
     hideInMenu?: boolean; // 是否隐藏改菜单
-}
+} & LoadingProps & LoaderProps;
 
 /**
  * 暴露入口
@@ -18,8 +28,6 @@ export type keepAliveType = 'auto' | 'force' | 'not';
 export declare type RouteWithModuleRoutesProps = {
     routers: any[];
     onRouteChange?: (route: RouteProps & RrefetchRoute) => void;
-    isVite?: boolean; // 是否使用vite模式
-    loading?: boolean | ReactNode; // 是否使用loading效果  false不使用 true使用默认的 也可传递组件
     /**
      * auto 默认缓存非hideInMenu菜单级别的路由
      * force 强制缓存所有页面
@@ -28,7 +36,7 @@ export declare type RouteWithModuleRoutesProps = {
     keepAlive?: keepAliveType;
     // 卸载缓存的的路由  传递过来的是路由地址
     uninstallKeepAliveKeys?: Array<string>;
-}
+} & LoadingProps & LoaderProps;
 
 export type SingleRouterProps = {
     router: RouteProps & RrefetchRoute;
