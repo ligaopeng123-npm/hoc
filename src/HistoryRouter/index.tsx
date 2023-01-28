273b3be6d4b7d706ab9d387d4b988a7cb08f683d
ligaopeng123 <1170234932@qq.com>
2023-01-28 13:34:47 +0800

    HistoryRouterProps

--- src/HistoryRouter/index.tsx
+++ src/HistoryRouter/index.tsx
@@ -9,7 +9,7 @@
  * @date: 2022/4/9 19:08
  *
  **********************************************************************/
-import { createElement, useLayoutEffect, useState } from 'react';
+import { createElement, PropsWithChildren, useLayoutEffect, useState } from 'react';
 import { createBrowserHistory } from 'history';
 import { Router } from 'react-router-dom';
 
@@ -17,10 +17,9 @@ export const history = createBrowserHistory();
 
 interface HistoryRouterProps {
     history: typeof history;
-    children?: any;
 }
 
-const HistoryRouter = ({history, children}: HistoryRouterProps) => {
+const HistoryRouter = ({history, children}: PropsWithChildren<HistoryRouterProps>) => {
     const [state, setState] = useState({
         action: history.action,
         location: history.location
